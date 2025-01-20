0e1cb59246d1bc99e1ebf02416815c88dc996e2b
石原 祐希 <issi0430bjc@gmail.com>
2025-01-20 15:57:58 +0900

    practiceモジュールにLoggerMiddlewareを適用

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PracticeModule } from './practice/practice.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { PracticeController } from './practice/practice.controller';
 
 @Module({
   imports: [PracticeModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(PracticeController);
+  }
+}
