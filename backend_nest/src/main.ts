e6478d122e95fd17d7dc0d8b16c781b982839c97
石原 祐希 <issi0430bjc@gmail.com>
2025-01-30 18:28:54 +0900

    backend側にcorsを設定

--- backend_nest/src/main.ts
+++ backend_nest/src/main.ts
@@ -5,9 +5,10 @@ import { PracticeInterceptor } from './common/interceptors/practice/practice.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // TODO: 詳細に設定したい
   app
     .useGlobalFilters(new HttpExceptionFilter())
     .useGlobalInterceptors(new PracticeInterceptor());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
