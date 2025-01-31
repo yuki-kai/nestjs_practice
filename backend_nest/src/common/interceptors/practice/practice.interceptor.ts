9757d9563a5fd7518cd4d3884b2f3814c68cec56
石原 祐希 <issi0430bjc@gmail.com>
2025-02-01 03:21:27 +0900

    backendの戻り値の型を調整

--- backend_nest/src/common/interceptors/practice/practice.interceptor.ts
+++ backend_nest/src/common/interceptors/practice/practice.interceptor.ts
@@ -1,16 +1,33 @@
 import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
+
+export const Info = {
+  statusCode: 200,
+  message: 'success'
+}
+
+/**
+* export type 型エイリアスのexport
+* オブジェクトの値のユニオン型を作成
+* typeof xxx &  型定義をマージして返却
+*/
+export type Response<T> = typeof Info & {
+  data: T
+}
 
 @Injectable()
-export class PracticeInterceptor implements NestInterceptor {
+export class PracticeInterceptor<T> implements NestInterceptor<T, Response<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('--- Interceptor Start ---');
     return next.handle().pipe(
-      tap(() => {
+      map(data => {
         console.log('--- Interceptor End ---');
+        console.log(data);
+        return Object.assign({}, Info, {data})
       }),
       catchError((error) => {
         console.log('--- Interceptor Error ---');
+        console.log(error);
         return throwError(() => new HttpException(error.message, HttpStatus.BAD_REQUEST));
       })
     );
