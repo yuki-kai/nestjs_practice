591af01e71e3dcc273788c75384d5b695419fd27
石原 祐希 <issi0430bjc@gmail.com>
2025-02-15 00:20:53 +0900

    backendをhttps化

--- backend_nest/src/main.ts
+++ backend_nest/src/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { PracticeInterceptor } from './common/interceptors/practice/practice.interceptor';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions,
     cors: {
       "origin": ["https://localhost:3000"],
       "methods": "PUT,PATCH,DELETE",
